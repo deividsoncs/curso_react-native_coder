f357a81c26ac72e0567ce889537e4e8069b13077
Deividson Calixto <deividsoncs@gmail.com>
2019-11-06 19:48:02 -0300

    primeiro commit do curso

---
+++ exercicios/src/componentes/ParImpar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import Padrao from "../estilo/Padrao"
+import If from './If'
+
+function parOuImpar(num){    
+    const v = num % 2 == 0 ? 'par'  : 'Ímpar'
+    return <Text style={Padrao.ex}>{v}</Text>
+
+}
+
+export default props => 
+    <View>
+        {/* {parOuImpar(props.numero)} */}
+        {
+            // props.numero % 2 == 0 
+            // ? <Text style={Padrao.ex}>Par</Text>
+            // : <Text style={Padrao.ex}>Ímpar</Text>
+        }        
+        <If test={props.numero % 2 == 0}>
+            <Text style={Padrao.ex}>Par</Text>
+        </If>
+        <If test={props.numero % 2 != 0}>
+            <Text style={Padrao.ex}>Ímpar</Text>
+        </If>
+    </View>
